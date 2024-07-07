19b0873540516911f034e626e7411bc84ad3c240
Vlad <vladbolkunovv@gmail.com>
2024-07-07 17:29:59 +0300

    feat: logger

---
+++ src/util/logger.ts
@@ -0,0 +1,22 @@
+import winston from "winston";
+import process from "process";
+import { DateTime } from "luxon";
+
+export const logger = winston.createLogger({
+  level: "debug",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.colorize(),
+    winston.format.printf(
+      (info) =>
+        `[App - ${process.pid}] ` +
+        `${DateTime.fromISO(info.timestamp).toLocaleString({
+          timeStyle: "medium",
+          dateStyle: "short",
+        })} ` +
+        `${info.level} ` +
+        `${info.message}`,
+    ),
+  ),
+  transports: [new winston.transports.Console()],
+});
