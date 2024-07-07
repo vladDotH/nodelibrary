03cb3a0cf73a1ae70bb495ae9ad978fe94fb3a73
Vlad <vladbolkunovv@gmail.com>
2024-07-07 18:22:05 +0300

    feat: errors handling

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,19 @@ import bodyParser from "koa-bodyparser";
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    logger.error(err.message);
+    logger.verbose(err.stack?.toString());
+    ctx.status = err.status ?? 500;
+    ctx.body = err.message;
+  }
+});
+
 app.use(bodyParser());
+
 app.use(apiController.routes());
 app.listen(configService.PORT, configService.HOST);
 logger.info(`App started in ${configService.APP_URL}`);
