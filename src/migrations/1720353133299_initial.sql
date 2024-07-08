31ab692942082f7f864188ca0d8bae5fc15877af
Vlad <vladbolkunovv@gmail.com>
2024-07-08 21:51:41 +0300

    feat(db): user indexes & constraints

--- src/migrations/1720353133299_initial.sql
+++ src/migrations/1720353133299_initial.sql
@@ -1,21 +1,26 @@
 -- Up Migration
 create table "Books"
 (
-    id              serial primary key,
-    title           varchar,
-    author          varchar,
-    publicationDate date,
-    genres          varchar[]
+    id                serial primary key,
+    title             varchar,
+    author            varchar,
+    "publicationDate" date,
+    genres            varchar[]
 );
 create table "Users"
 (
-    id           serial primary key,
-    username     varchar,
-    passwordHash varchar,
-    rolesMask    int4,
-    email        varchar,
-    emailToken   varchar
+    id               serial primary key,
+    username         varchar unique,
+    "passwordHash"   varchar,
+    "rolesMask"      int4,
+    email            varchar unique,
+    "emailToken"     varchar,
+    "emailConfirmed" bool,
 );
+
+create index Users_username_index on "Users" using hash (username);
+create index Users_email_index on "Users" using hash (email);
+create index Users_email_token_index on "Users" using hash ("emailToken");
 
 -- Down Migration
 drop table if exists "Books";
